4991e0c710be0f89882f49c5a8280df1e718aabc
rikuto <92492980+rurikunn@users.noreply.github.com>
2023-04-14 14:49:34 +0900

    simizu
    EnemyCreate

---
+++ TZ 2/Assets/simizu/s_script/EnemyCreate_Up.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreate_Up : MonoBehaviour
+{
+	public GameObject enemy; //作るプレハブ：Inspectorで指定
+	public float intervalSec = 1; //作成間隔（秒）：Inspectorで指定する
+	//public AudioClip bossbulletsou;
+	Transform tf; //敵のTransform
+
+	void Start()
+	{
+		//Debug.Log(attack_flag);
+
+		//指定秒ごとに、CreatePrefabをくり返し実行する予約
+		InvokeRepeating("CreatePrefab", intervalSec, intervalSec);
+	}
+
+	void Update()
+	{
+
+	}
+
+	void CreatePrefab()
+	{
+		//このオブジェクトの範囲内にランダムに
+		Vector3 area = GetComponent<SpriteRenderer>().bounds.size;
+
+		Vector3 newPos = this.transform.position;
+		newPos.x = Random.Range(-8, 9);
+		newPos.y = -8;
+		newPos.z = 1; //手前に表示
+
+		//プレハブを作る
+		GameObject newGameObject = Instantiate(enemy) as GameObject;
+		newGameObject.transform.position = newPos;
+
+		//var audioSource = FindObjectOfType<AudioSource>();
+		//audioSource.PlayOneShot(bossbulletsou);
+	}
+}
