4f000772209af1d7ca3b026aff94b36dcf7472a1
umauam <89572709+umauam@users.noreply.github.com>
2023-04-19 16:30:03 +0900

    tuzi
    完成

--- TZ 2/Assets/tuzinkusu/sukuriputo/Player.cs
+++ TZ 2/Assets/tuzinkusu/sukuriputo/Player.cs
@@ -8,10 +8,22 @@ public class Player : MonoBehaviour
 
 	//敵キャラがGameManagerを介して、プレイヤーの位置情報を読み取れるようにpublicにしている。
 	public Transform tf;
+	public Transform tf2;
+	public Transform tf3;
+	public Transform tf4;
+
 
 	void Awake()
 	{
 		tf = transform;
+
+		tf2 = transform;
+
+		tf3 = transform;
+
+		tf4 = transform;
+
+
 	}
 
 	//移動やスプライトの更新部分は割愛。    
